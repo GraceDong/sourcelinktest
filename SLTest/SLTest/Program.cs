96a37c3fd4278089f986ed655a7563a42a5821ad
VSEQA2 <vseqa2@microsoft.com>
2017-07-21 01:18:44 -0700

    Project created

---
+++ SLTest/SLTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int para = 1;
+            para++;
+            Console.WriteLine(para);
+        }
+    }
+}
